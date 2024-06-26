dd54f37360e83ee20ebfd04309b46d55c592dd4c
Your Name <dgebuadzeirakli28@gmail.com>
2024-06-26 14:24:41 +0400

    refactoring

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IconComponent } from '../helpers/icon/icon.component';
 
@@ -12,21 +12,40 @@ import { IconComponent } from '../helpers/icon/icon.component';
         <label for="input">შეიყვანეთ დასახელება</label>
         <input class="input" id="input" type="text" />
       </div>
-      <div class="dropdown" (click)="toggleDropdown()">
-        <span class="choose-status">აირჩიეთ დამატების სტატუსი</span>
-        <app-icon class="icon" [imagePath]="'/arrow-down.svg'"></app-icon>
+      <div class="dropdown" (click)="toggleDropdown($event)">
+        <span
+          class="choose-status"
+          [ngClass]="{
+            'status-orange': selectedStatus === 'მიმდინარე სტატუსი',
+            'status-sky': selectedStatus === 'დასრულებული სტატუსი'
+          }"
+        >
+          {{ selectedStatus || 'აირჩიეთ დამატების სტატუსი' }}
+        </span>
+        <app-icon
+          class="icon"
+          [imagePath]="dropdownOpen ? '/arrow-down.svg' : '/arrow-up.svg'"
+        ></app-icon>
         <div *ngIf="dropdownOpen" class="dropdown-menu">
           <div
-            class="dropdown-item"
-            (click)="selectStatus('მიმდინარე სტატუსი')"
+            class="dropdown-item-orange"
+            (click)="selectStatus('მიმდინარე სტატუსი', 'iconOrange')"
           >
-            მიმდინარე სტატუსი
+            <span>მიმდინარე სტატუსი</span>
+            <app-icon
+              *ngIf="selectedIcon === 'iconOrange'"
+              [imagePath]="'/iconOrange.svg'"
+            ></app-icon>
           </div>
           <div
-            class="dropdown-item"
-            (click)="selectStatus('დასრულებული სტატუსი')"
+            class="dropdown-item-sky"
+            (click)="selectStatus('დასრულებული სტატუსი', 'iconSky')"
           >
-            დასრულებული სტატუსი
+            <span>დასრულებული სტატუსი</span>
+            <app-icon
+              *ngIf="selectedIcon === 'iconSky'"
+              [imagePath]="'/iconSky.svg'"
+            ></app-icon>
           </div>
         </div>
       </div>
@@ -42,13 +61,23 @@ import { IconComponent } from '../helpers/icon/icon.component';
 export class AddComponent {
   dropdownOpen = false;
   selectedStatus: string | null = null;
+  selectedIcon: string | null = null;
 
-  toggleDropdown() {
+  toggleDropdown(event: MouseEvent) {
+    event.stopPropagation();
     this.dropdownOpen = !this.dropdownOpen;
   }
 
-  selectStatus(status: string) {
+  selectStatus(status: string, icon: string) {
     this.selectedStatus = status;
-    this.dropdownOpen = false;
+    this.selectedIcon = icon;
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: Event) {
+    const target = event.target as HTMLElement;
+    if (!target.closest('.dropdown')) {
+      this.dropdownOpen = false;
+    }
   }
 }

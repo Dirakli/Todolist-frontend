fa2f6003771aebcf264380304a0bcfcb398fc5c8
Your Name <dgebuadzeirakli28@gmail.com>
2024-06-26 14:30:27 +0400

    testing

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -36,6 +36,7 @@ import { IconComponent } from '../helpers/icon/icon.component';
               *ngIf="selectedIcon === 'iconOrange'"
               [imagePath]="'/iconOrange.svg'"
             ></app-icon>
+            <span>Hello World!</span>
           </div>
           <div
             class="dropdown-item-sky"
